54ca89ec233c1aec7ff58f39d94109e3ea55040e
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-06-22 22:51:12 -0500

    minor changes to style.css to allow for use in smaller screens

--- script.js
+++ script.js
@@ -1,89 +1,61 @@
-$(document).ready(function() {
-  var currentDay = moment().format("dddd MMMM Do YYYY");
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+// ready function to load the page and get the current time and date from moment.js and display it on the page
+$(document).ready(function () {
+    // saveBtn click listener and save to local storage
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-  console.log(currentDay);
-
-  var displayDay = function() {
-    console.log("Reached displayDay()");
-    $("#currentDay").text(currentDay);
-    console.log("Current day:", currentDay);
-  };
-
-  var currentHour = moment().format("h A");
-
-  console.log("Current hour:", currentHour);
-
-  $(document).on("click", ".saveBtn", function() {
-    console.log("Save button clicked!");
-    // Get nearby values of the description in jQuery
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
-    // Save text in local storage
-    localStorage.setItem(time, text);
-    console.log("Reached timeTracker()");
-  });
-
-  function timeTracker() {
-    //get current number of hours.
-    var timeNow = moment().hour();
-
-    // loop over time blocks
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-        // To check the time and add the classes for background indicators
-        if (blockTime < timeNow) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (blockTime === timeNow) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-
-        }
+        // Save text in local storage
+        localStorage.setItem(time, text);
     })
-}
+//    Time tracker function to track the time and change the background color of the time blocks
+    function timeTracker() {
+        //get current number of hours.
+        var timeNow = moment().hour();
 
-  // Get item from local storage if any
-  var hour8Value = localStorage.getItem("hour8");
-  console.log("Retrieved value for hour8:", hour8Value);
-  var expectedHour8Value = "expected value for hour8";
-  if (hour8Value === expectedHour8Value) {
-    console.log("Retrieved value for hour8 matches the expected value.");
-  } else {
-    console.log("Retrieved value for hour8 does not match the expected value.");
-  }
-  $("#hour8 .description").val(hour8Value);
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-  var hour9Value = localStorage.getItem("hour9");
-  console.log("Retrieved value for hour9:", hour9Value);
-  var expectedHour9Value = "expected value for hour9";
-  if (hour9Value === expectedHour9Value) {
-    console.log("Retrieved value for hour9 matches the expected value.");
-  } else {
-    console.log("Retrieved value for hour9 does not match the expected value.");
-  }
-  $("#hour9 .description").val(hour9Value);
+            // To insepct the time blocks and see if we've moved past this time
+            // Add and remove classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-  // Repeat the above steps for each hour
+            }
+        })
+    }
 
-  timeTracker();
-  console.log("Reached the function calls");
-
-  displayDay();
-
-  // Call timeTracker() every minute
-  // setInterval(timeTracker, 60000);
-});
-
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+// call the time tracker function
+    timeTracker();
+})
 
 
 
